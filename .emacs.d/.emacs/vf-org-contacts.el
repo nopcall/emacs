1c3c0017e80abb2f1ff4a267e30e2a9760283291
visayafan <visayafan@gmail.com>
2013-05-16 13:19:45 +0800

    org-contacts

--- .emacs.d/.emacs/vf-org-contacts.el
+++ .emacs.d/.emacs/vf-org-contacts.el
@@ -2,14 +2,16 @@
 (require 'org-contacts)
 (setq fan/org-contacts-file-location
       (if (eq system-type 'gnu/linux)
-          "~/d/Dropbox/others/org-contacts-file.org"
-        "D:/Dropbox/others/org-contacts-file.org"))
-(setq org-contacts-files fan/org-contacts-file-location)
+          "/home/visayafan/d/Dropbox/others/contacts.org"
+        "D:/Dropbox/others/contacts.org"))
+(add-to-list 'org-contacts-files fan/org-contacts-file-location)
 
-(add-to-list 'org-capture-templates
-             '("c" "Contacts" entry (file fan/org-contacts-file-location)
-               "** %(org-contacts-template-name)
-                :PROPERTIES
-                :EMAIL: %(org-contacts-template-email)
-                :END:"))
+(eval-after-load "org-capture"
+  '(progn
+     (add-to-list 'org-capture-templates
+                  '("c" "Contacts" entry (file fan/org-contacts-file-location)
+                    "* %(org-contacts-template-name)
+:PROPERTIES:
+:EMAIL: %(org-contacts-template-email)
+:END:"))))
 
--- .emacs.d/.emacs/vf-org-setting-common.el
+++ .emacs.d/.emacs/vf-org-setting-common.el
@@ -4,18 +4,17 @@
 ;; (require 'vf-org-latex)      ;;;; org里的latex配置 
 (load-file "~/.emacs.d/.emacs/vf-org-gtd.el") ;;; org里gtd配置 
 ;; (require 'org-publish)                        ;;; org-publish 配置 
+(global-set-key "\C-coc" 'org-capture)
 (add-hook 'org-mode-hook
           '(lambda ()
              (turn-on-font-lock)
              (setq truncate-lines nil)  ;解决org-mode在编辑中文的时候不会自动折行的问题
              (define-key org-mode-map "\C-col" 'org-store-link)
-             (define-key org-mode-map "\C-coc" 'org-capture)
              (define-key org-mode-map "\C-coa" 'org-agenda)
              (define-key org-mode-map "\C-cob" 'org-iswitchb)            ;switch only between files with org suffix
              (define-key org-mode-map "\C-cot" 'org-toggle-link-display) ;toogle link display, literal or descriptive
              (setq org-export-default-language "zh-CN")
-             (setq org-export-language-setup (quote (("zh-cn" "作者" "日期" "目录" "脚注") ("en" "Author" "Date" "Table of Contents" "Footnotes"))))
-             ))
+             (setq org-export-language-setup (quote (("zh-cn" "作者" "日期" "目录" "脚注") ("en" "Author" "Date" "Table of Contents" "Footnotes"))))))
 ;; org-babel
 ;; active Babel languages
 (org-babel-do-load-languages
